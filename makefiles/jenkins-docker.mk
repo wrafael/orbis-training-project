61a75f5800aa2234472e77e2ca906ddf3e1a2db3
Walter Rafael <walter.rafael@metricaandina.com>
2018-10-26 10:49:35 -0500

    feat(docker): jenkins

--- makefiles/jenkins-docker.mk
+++ makefiles/jenkins-docker.mk
@@ -3,6 +3,7 @@
 JENKIS_NAME = jenkins_name
 JENKIS_NETWORK = jenkis_net
 JENKIS_VOLUMEN = jenkins_volumen
+PORT_NODE = 1042
 
 docker.build:
 	docker build -t ${USER_DOCKER}/jenkins-deploy:1.0.0 docker/jenkins
@@ -19,8 +20,11 @@ jenkins-install:
 jenkins-start:
 	docker network create ${JENKIS_NETWORK}
 	docker run -d --net=${JENKIS_NETWORK} \
-	--name ${JENKIS_NAME} -p 3030:1042 \
+	--name ${JENKIS_NAME} -p 3030:${PORT_NODE} \
 	--volumes-from ${JENKIS_VOLUMEN} -w /app ${DOCKER_IMAGE} npm start
 
 jenkins-curl:
-	echo "test"
+	echo "CURL"
+	docker run -t  --rm --net=${JENKIS_NETWORK} \
+	--volumes-from ${JENKIS_VOLUMEN} -w /app ${DOCKER_IMAGE} \
+	curl ${JENKIS_NAME}:${PORT_NODE}
